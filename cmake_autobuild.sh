567e4d4a6b6f9fa770fa865e49397ddfd95e3d8e
Gleb <46938gt@rambler.ru>
2024-02-04 05:12:23 +0300

    Autobuild CMake SQLite3

---
+++ cmake_autobuild.sh
@@ -0,0 +1,16 @@
+﻿#!/bin/bash
+
+folder_path="./build"
+
+if [ -d "$folder_path" ]; then
+    echo "Папка уже существует."
+else
+    mkdir -p "$folder_path"
+    echo "Папка создана."
+fi
+
+cd ./build
+
+cmake ..
+
+cmake --build . --verbose > ../logs/Cmake_build_shell.log 2>&1
