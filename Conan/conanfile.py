65139770aae737d90e91a33e3bd3b54ddab79e29
Gleb <46938gt@rambler.ru>
2024-02-04 05:19:49 +0300

    ConanFile config

---
+++ Conan/conanfile.py
@@ -0,0 +1,11 @@
+﻿from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps
+
+class DevOpsConan(ConanFile):
+    name = "boost_conan"
+    settings = "os", "compiler", "build_type", "arch"
+    version = "1.0"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires("fmt/10.2.1")
