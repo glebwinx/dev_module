b164021c1903b2973d465f80fa1f3b2c655ab42e
Gleb <46938gt@rambler.ru>
2024-02-04 05:04:02 +0300

    Проверка sqlite

---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "src/sqlite3.h"
+using namespace std;
+
+int main()
+{
+    sqlite3* db;
+    int rc;
+    rc = sqlite3_open("Test.db", &db);
+    if (rc != SQLITE_OK)
+        cout << "file is not open" << endl;
+    else
+        cout << "file open" << endl;
+    return 0;
+}
