3e5e65d7ea94aee7dcf4c0418ac04d3d308ae452
Gleb <46938gt@rambler.ru>
2024-02-04 05:33:09 +0300

    Installing Linux lib

---
+++ OS/Linux/inst_required_lib.sh
@@ -0,0 +1,24 @@
+﻿if ! command -v virtualbox &> /dev/null; then
+	sudo apt-get update
+	sudo apt-get install virtualbox -y
+fi
+
+if ! command -v vagrant &> /dev/null; then
+	sudo apt-get install vagrant -y
+fi
+
+if ! command -v git &> /dev/null; then 
+	sudo apt-get install git -y
+fi
+
+if ! command -v python &> /dev/null; then 
+	sudo apt-get install python -y
+fi
+
+if ! command -v pip &> /dev/null; then	
+	sudo apt-get install pip -y 
+fi
+
+if ! command -v ansible &> /dev/null; then 
+	sudo pip install ansible
+fi
